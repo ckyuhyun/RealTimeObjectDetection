66c77367f79762e9efff26cf0ee1cbe55ac3bdc5
ckyuhyun <ckyuhyun@gmail.com>
2023-09-15 12:03:46 -0400

    commit test

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import pyautogui
 import matplotlib.pyplot as plt
 from PIL import Image
 import PIL
-# model.add(layers.Dense(64, activation=opt))
 
 image = pyautogui.screenshot()
 image.save(f'{os.getcwd()}/image.jpg')
